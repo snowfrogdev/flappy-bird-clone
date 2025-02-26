700ddf4b087c56e4022c23bf1a4c08bf034f6b51
Philippe Vaillancourt <phil@snowfrog.dev>
2025-02-26 15:07:17 +0000

    Add flap mechanic to player character and configure input settings

--- player.gd
+++ player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+@export var FLAP_STRENGTH: float = -300.0
 
-	move_and_slide()
+func _physics_process(delta: float) -> void:
+  if not is_on_floor():
+    velocity += get_gravity() * delta
+  
+  if Input.is_action_just_pressed("Flap Wings"):
+    velocity.y = FLAP_STRENGTH
+  move_and_slide()
