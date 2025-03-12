a28cc713debed07324f6dad7db6f08f655f4110c
Philippe Vaillancourt <phil@snowfrog.dev>
2025-03-12 17:03:04 +0000

    Add flap sound effect to player and update scene configuration

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var FLAP_STRENGTH: float = -300.0
 @export var TILT_FACTOR: float = 0.001
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var flap_sound: AudioStreamPlayer = $WingsFlapSfx
 
 signal player_collided()
 
@@ -15,6 +16,8 @@ func _physics_process(delta: float) -> void:
   
   if Input.is_action_just_pressed("Flap Wings"):
     velocity.y = FLAP_STRENGTH
+    flap_sound.pitch_scale = 1.0 + randf_range(-0.05, 0.05)
+    flap_sound.play()
   
   rotation = clamp(velocity.y * TILT_FACTOR, deg_to_rad(-20), deg_to_rad(20))
   
